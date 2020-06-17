cec6d748bcee6fc45e768b12a2f4a4227131a47f
paarthsoin <paarth.soin9@gmail.com>
2020-06-18 04:10:56 +0530

    Remove unnessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side Js file");
-
 var weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne=document.querySelector("#message-1");
